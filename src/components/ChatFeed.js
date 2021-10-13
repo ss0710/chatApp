0cba75ee39b5deb7cd28f1f665c95e5307438ad5
ss0710 <sudheersid0710@gmail.com>
2021-10-13 20:57:11 +0530

    initiazed project

---
+++ src/components/ChatFeed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChatFeed = (props) => {
+    console.log(props)
+    return (
+        <div>
+            chatFeed
+        </div>
+    )
+}
+
+export default ChatFeed;
---
+++ src/components/MyMessage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyMessage = () => {
+    return (
+        <div>
+            my message
+        </div>
+    )
+}
+
+export default MyMessage;
