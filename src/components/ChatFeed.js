4bf0ef0746dc207e6e48794f07708e60e00aeb1a
ss0710 <sudheersid0710@gmail.com>
2021-10-13 21:44:31 +0530

    added props

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -4,7 +4,30 @@ import MyMessage from './MyMessage';
 import ThierMessages from './ThierMessages';
 
 const ChatFeed = (props) => {
-    console.log(props)
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index)=>{
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index-1];
+            const isMyMessage = userName === message.sender.username;
+
+            return (
+                <div key={`msg_${index}`} stylr={{ width: '100%'}}>
+                    <div className="message-block">
+                        
+                    </div>
+                </div>
+            );
+        })
+    }
+
+    renderMessages();
+
     return (
         <div>
             chatFeed
